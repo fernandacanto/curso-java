c1eeceba65f2277570ce4cce943cc6fa17135147
Fernanda do Canto Nogueira <fernanda.canto.ngr@gmail.com>
2019-08-16 08:53:43 -0300

    Criando um pacote.

--- aula08/src/com/fernanda/cursojava/aula10/Variaveis.java
+++ aula08/src/com/fernanda/cursojava/aula10/Variaveis.java
@@ -1,4 +1,11 @@
 package com.fernanda.cursojava.aula10;
 
 public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 26;
+        String nome = "Fernanda";
+
+        System.out.println("Nome " + nome);
+        System.out.println("Nome " + nome);
+    }
 }
